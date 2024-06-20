1b89c2c488329b0e1159af74fb693577d5d8326e
willpoqigithub <166766848+willpoqigithub@users.noreply.github.com>
2024-06-20 12:34:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,161 +1,165 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-const total = new Map();
-app.get('/total', (req, res) => {
-  const data = Array.from(total.values()).map((link, index)  => ({
-    session: index + 1,
-    url: link.url,
-    count: link.count,
-    id: link.id,
-    target: link.target,
-  }));
-  res.json(JSON.parse(JSON.stringify(data || [], null, 2)));
-});
-app.get('/', (res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-app.post('/api/submit', async (req, res) => {
-  const {
-    cookie,
-    url,
-    amount,
-    interval,
-  } = req.body;
-  if (!cookie || !url || !amount || !interval) return res.status(400).json({
-    error: 'Missing state, url, amount, or interval'
-  });
-  try {
-    const cookies = await convertCookie(cookie);
-    if (!cookies) {
-      return res.status(400).json({
-        status: 500,
-        error: 'Invalid cookies'
-      });
-    };
-    await share(cookies, url, amount, interval)
-    res.status(200).json({
-      status: 200
-    });
-  } catch (err) {
-    return res.status(500).json({
-      status: 500,
-      error: err.message || err
-    });
-  }
-});
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Share Boost</title>
+   </head>
+   <body>
+      <div class="wrapper-container">
+         <div class="container">
+            <h1>SHARE BOOST</h1>
+            <p>A user-friendly automated website that utilizes your browser's cookies to streamline the process of sharing posts on Facebook using the provided URL.</p>
+            <div class="container">
+         <div class="form-wrapper">
+            <form onsubmit="submitForm(event)" class="form-container">
+               <div class="form-item">
+                  <label for="cookies">APPSTATE:</label>
+                  <input type="text" id="cookies" name="cookie" placeholder="">
+               </div>
+               <div class="form-item">
+                  <label for="urls">LINK:</label>
+                  <input type="text" id="urls" name="url" placeholder="">
+               </div>
+               <div class="form-item">
+                  <label for="amounts">LIMIT:</label>
+                  <input type="text" id="amounts" name="amount" placeholder="">
+               </div>
+               <div class="form-item">
+                  <label for="intervals">DELAY:</label>
+                  <input type="text" id="intervals" name="interval" placeholder="" pattern="[0-9]*">
+               </div>
+               <div class="form-item">
+                  <button type="submit" onclick="submitForm()" id="submit-button">Submit</button>
+               </div>
+               </div>
+               <div class="form-item">
+                  <div id="result"></div>
+               </div>
+            </form>
+         </div>
+         <div class="container" id="process-container">
+            <h1>SHARE BOOST</h1>
+            <h4>PROCESSING:</h4>
+            <div id="processing">
+            </div>
+         </div>
+         </div>
+      </div>
+      <style>
+         body, html {
+         margin: 0;
+         }
+         body {
+         padding: 20px;
+         font-family: 'Poppins', sans-serif;
+         background-image: url("https://i.imgur.com/Yus9p8c.jpeg");
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         }
+         .container {
+         background-color: darkslategrey;
+         border: solid 1px white;
+        border-radius: 10px;
+         padding: 20px;
+         margin: 20px auto;
+         overflow: auto;
+    max-width: 100%;
+    max-height: 40%;
+    margin-bottom: 20px;
+    padding: 20px;
+    margin: 20px auto;
+    cursor: default;
+    outline: 1px solid #fff;
+    background: ;
+    border-radius: 10px;
+    animation: animate 1.5s linear infinite;
+    border: 1px solid transparent;
+    animation: neon-border 2s linear infinite alternate;
+}
 
-async function share(cookies, url, amount, interval) {
-  const id = await getPostID(url);
-  const accessToken = await getAccessToken(cookies);
-  if (!id) {
-    throw new Error("Unable to get link id: invalid URL, it's either a private post or visible to friends only");
-  }
-  const postId = total.has(id) ? id + 1 : id;
-  total.set(postId, {
-    url,
-    id,
-    count: 0,
-    target: amount,
-  });
-  const headers = {
-    'accept': '*/*',
-    'accept-encoding': 'gzip, deflate',
-    'connection': 'keep-alive',
-    'content-length': '0',
-    'cookie': cookies,
-    'host': 'graph.facebook.com'
-  };
-  let sharedCount = 0;
-  let timer;
-  async function sharePost() {
-    try {
-      const response = await axios.post(`https://graph.facebook.com/me/feed?link=https://m.facebook.com/${id}&published=0&access_token=${accessToken}`, {}, {
-        headers
-      });
-      if (response.status !== 200) {
-      } else {
-        total.set(postId, {
-          ...total.get(postId),
-          count: total.get(postId).count + 1,
-        });
-        sharedCount++;
-      }
-      if (sharedCount === amount) {
-        clearInterval(timer);
-      }
-    } catch (error) {
-      clearInterval(timer);
-      total.delete(postId);
+@keyframes neon-border {
+    from {
+        border-color: yellow;
+        box-shadow: 0 0 10px yellow, 0 0 20px yellow, 0 0 30px yellow;
+              }
+    to {
+        border-color: red;
+        box-shadow: 0 0 20px red, 0 0 30px red, 0 0 40px red;
     }
-  }
-  timer = setInterval(sharePost, interval * 1000);
-  setTimeout(() => {
-    clearInterval(timer);
-    total.delete(postId);
-  }, amount * interval * 1000);
 }
-async function getPostID(url) {
-  try {
-    const response = await axios.post('https://id.traodoisub.com/api.php', `link=${encodeURIComponent(url)}`, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
-    return response.data.id;
-  } catch (error) {
-    return;
-  }
-}
-async function getAccessToken(cookie) {
-  try {
-    const headers = {
-      'authority': 'business.facebook.com',
-      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
-      'cache-control': 'max-age=0',
-      'cookie': cookie,
-      'referer': 'https://www.facebook.com/',
-      'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Linux"',
-      'sec-fetch-dest': 'document',
-      'sec-fetch-mode': 'navigate',
-      'sec-fetch-site': 'same-origin',
-      'sec-fetch-user': '?1',
-      'upgrade-insecure-requests': '1',
-    };
-    const response = await axios.get('https://business.facebook.com/content_management', {
-      headers
-    });
-    const token = response.data.match(/"accessToken":\s*"([^"]+)"/);
-    if (token && token[1]) {
-      const accessToken = token[1];
-      return accessToken;
-    }
-  } catch (error) {
-    return;
-  }
-}
-async function convertCookie(cookie) {
-  return new Promise((resolve, reject) => {
-    try {
-      const cookies = JSON.parse(cookie);
-      const sbCookie = cookies.find(cookies => cookies.key === "sb");
-      if (!sbCookie) {
-        reject("Detect invalid appstate please provide a valid appstate");
-      }
-      const sbValue = sbCookie.value;
-      const data = `sb=${sbValue}; ${cookies.slice(1).map(cookies => `${cookies.key}=${cookies.value}`).join('; ')}`;
-      resolve(data);
-    } catch (error) {
-      reject("Error processing appstate please provide a valid appstate");
-    }
-  });
-}
-app.listen(5000)
+
+         .form-item {
+         padding: 8px;
+         }
+         h1 {
+         color: whitesmoke;
+         text-align: center;
+         }
+         h4 {
+           text-align: center;
+         }
+         p {
+           text-align: center;
+           color: whitesmoke;
+         }
+         .form-item label {
+         font-size: 13px;
+         color: white;
+         margin-bottom: 5px; 
+         display: block; 
+         }
+         input, button {
+         background: ghostwhite;
+         color: black; 
+         border: solid 1px #222;
+         box-sizing: border-box;
+         width: 100%;
+         margin-bottom: 10px;
+         padding: 10px;
+         border-radius: 4px;
+         font-size: 11px;
+         }
+         button {
+         cursor: none;
+         color: #0a0a0a;
+         background-color: #ededed;
+         }
+         button:hover {
+         background: #CCC;
+         }
+         #result {
+         display: none;
+         text-align: center;
+         background-color: #ededed;
+         border: solid 1px #222;
+         padding: 10px;
+         border-radius: 4px;
+         color: #0a0a0a;
+         }
+         #process {
+         }
+         .current-online {
+         font-size: 13px;
+         background: #0a0a0a;
+         padding: 10px;
+         color: #898989;
+         border: solid 1px #898989;
+         border-radius: 4px;
+         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+         margin-bottom: 10px;
+         overflow: auto;
+         white-space: nowrap; 
+         }
+      </style>
+      <script>
+         async function submitForm() {
+           event.preventDefault();
+           const result = document.getElementById('result');
+           const button = document.getElementById('submit-button');
+           try {
+             result.style.display = 'block';
+             button.style.display = 'none';
+             const response
